766cf2f08d7a452d765274dad3c6766f4e3d8d1e
khaledhosni <meckawy@gmail.com>
2023-09-27 14:11:18 +0300

    ListView

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:psd_day5_network/data/remote/model/User.dart';
 import 'package:psd_day5_network/data/remote/service/UserService.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  var listOfUsers=[];
+  List<User> listOfUsers=[];
 
   @override
   void initState() {
@@ -22,15 +23,46 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   initData()async{
+    print('init data has been called');
     listOfUsers =await UserService().getAllUsers();
 
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("PSD"),
+      ),
 
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: listOfUsers.length,
+              itemBuilder: (context, index) {
 
+              return Card(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Image.network(listOfUsers[index].avatar!, width: 100,height: 100,fit: BoxFit.cover,),
+                    Column(
+                      children: [
+                        Text(listOfUsers[index].name!, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),textAlign: TextAlign.start,),
+                        Text(listOfUsers[index].phone!,style: TextStyle(fontSize: 16, ),textAlign: TextAlign.start),
+                        Text(listOfUsers[index].country!,style:TextStyle(fontSize: 16),textAlign: TextAlign.start,),
+                      ],
+                    )
+                  ],
+                ),
+              );
+
+            },),
+          )
+        ],
+      ),
     );
   }
 }
