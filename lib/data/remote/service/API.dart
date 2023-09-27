38f61425ae02ab0d3b7338151fdc6c392bca637f
khaledhosni <meckawy@gmail.com>
2023-09-27 11:56:49 +0300

    Singletone pattern

---
+++ lib/data/remote/service/API.dart
@@ -0,0 +1,18 @@
+
+import 'package:dio/dio.dart';
+
+class API{
+static final API instance= API._();
+
+  Dio dio= Dio();
+
+  API._(){
+    dio.options.connectTimeout=Duration(seconds: 15);
+    dio.options.baseUrl="https://618ebc2e50e24d0017ce141f.mockapi.io/";
+  }
+
+  factory API(){
+    return instance;
+  }
+
+}
