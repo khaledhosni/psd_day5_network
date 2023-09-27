d006e36caaca783108a9139fe575450d710063ca
khaledhosni <meckawy@gmail.com>
2023-09-27 11:05:45 +0300

    splash screen lifecycle

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_indicator/loading_indicator.dart';
+import 'package:psd_day5_network/ui/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
+
         body: Center(
           child: Container(
             child: SplashScreen() ,
@@ -22,26 +22,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    print('init state method has been called');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build method is started');
+
+
+    Future.delayed(Duration(seconds: 4),() {
+
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
+    },);
+
     return Column(
       mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Image.asset("images/logo.png",width: 128,height: 128,),
-        Text("Port Said Container and Cargo Handling Co."),
-        SizedBox(
-          width: 32,
-          height: 32,
-          child: LoadingIndicator(indicatorType: Indicator.ballRotate,
-          backgroundColor: Colors.white,
+        Container(child: Image.asset("images/logo.png",width: 128,height: 128,),
+          margin: EdgeInsets.only(top: 100,),
+        ),
+        Container(child: Text("Port Said Container and Cargo Handling Co."
+          ,style:GoogleFonts.delaGothicOne(
+            textStyle: TextStyle(
+                color: Colors.brown, fontSize: 24),
+          ),
+          textAlign: TextAlign.center,
+        ),
+          margin: EdgeInsets.only(top: 32),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(top: 300),
+          child: SizedBox(
+            width: 32,
+            height: 32,
+            child: LoadingIndicator(indicatorType: Indicator.ballRotate,
+              backgroundColor: Colors.white,
+              colors: [Colors.brown],
+            ),
           ),
         )
       ],
     );
   }
+
+  @override
+  void didUpdateWidget(covariant SplashScreen oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+
+    print('did update method ---> UI has been loaded');
+  }
+
+
+
 }
 
 
